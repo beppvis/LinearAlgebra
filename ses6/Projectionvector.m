24a1be677bcfe9d7edf7656331d27d1f8c94eb1f
beppvis <visruthvijind@gmail.com>
2025-03-23 18:19:06 +0530

    fixed now

--- ses6/Projectionvector.m
+++ ses6/Projectionvector.m
@@ -1,10 +1,7 @@
 %Write a progra m to find the projection of a  Vector in subsapce
 v=[0;1;0];
 A= [1 0 0;0 0 1;0 1 1];
-<<<<<<< HEAD
-=======
 P=A'*(A*A')\A; % since we use rows to represent vecotrs there is an interchange in formula
->>>>>>> 7b47186 (ses7)
 pro_v=P*v;
 disp('proj vector')
 disp(pro_v)
