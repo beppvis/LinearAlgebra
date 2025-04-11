c2ecb2650e43c623bfc6dc9620fa3b5fde07d248
beppvis <visruthvijind@gmail.com>
2025-04-11 09:56:01 +0530

    fixed column space

--- ses4/basis_for_columsnspace.m
+++ ses4/basis_for_columsnspace.m
@@ -1,16 +1,9 @@
 clc
 clear all
 A = [1 2 3; 4 5 6; 7 8 9];
-R = rref(A);
+[R,pivot_columns] = rref(A);
 [m,n] = size(R);
-pivot_columns =[];
 
-for j = 1:n
-    if any(R(:,j) ==1) % instead of another for loop any is used
-        pivot_coloumns = [pivot_columns,j]
-    end
-end
-
-B = A(:,pivot_coloumns)
+B = A(:,pivot_columns);
 disp('Column space of A is')
 disp(B)
